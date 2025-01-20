d3c4597ca50e1f81913e7f6a54653c33a802b51a
Saif Abdullah Mahi <Samahicse@gmail.com>
2025-01-21 05:38:36 +0600

    Paginations has been added to the views for the jobs

--- backend/job/views.py
+++ backend/job/views.py
@@ -4,18 +4,31 @@ from .models import Job
 from .serializers import JobSerializer
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
 from django.shortcuts import get_object_or_404
 from django.db.models import Avg , Min , Max , Count
-from asgiref.sync import async_to_sync
+from .filters import JobsFilter
 
 
 # Create your views here.
 
 @api_view(['GET'])
 def getAllJobs(request): 
-    jobs = Job.objects.all() 
-    serializer = JobSerializer(jobs , many = True)
-    return Response(serializer.data)
+
+    filterset = JobsFilter(request.GET , queryset=Job.objects.all().order_by('id')) 
+    count = filterset.qs.count()
+    #Pagination
+    resPerPage =3  
+    paginator = PageNumberPagination()
+    paginator.page_size = resPerPage
+    queryset = paginator.paginate_queryset(filterset.qs,request)
+
+    serializer = JobSerializer(queryset , many = True)
+    return Response({
+            "count" : count,
+            "resPerPage" : resPerPage ,
+            'jobs' : serializer.data 
+    })
 
 @api_view(['GET']) 
 def getJob(request , pk): 
